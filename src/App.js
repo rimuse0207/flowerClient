7579c796f098096853fc99bd2d159e97107b68c4
sleny <xkqortjddl@naver.com>
2020-02-14 20:17:27 +0900

    flowerClient

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Main from "./Main";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Main></Main>
+      <BrowserRouter>
+        <Main></Main>
+      </BrowserRouter>
     </div>
   );
 }
