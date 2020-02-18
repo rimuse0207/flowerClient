dba9ead9dee39b0f9bcd3bf28a493617314d9d05
sleny <xkqortjddl@naver.com>
2020-02-18 19:20:06 +0900

    flowerclient

--- src/App.js
+++ src/App.js
@@ -1,15 +1,85 @@
 import React from "react";
 import Main from "./Main";
-import { BrowserRouter } from "react-router-dom";
+import Login from "./Login/Login";
+import SignUp from "./Login/SignUp";
+import "./App.css";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import Modal from "react-awesome-modal";
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Main></Main>
-      </BrowserRouter>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibleLogin: false,
+      visibleSignUp: false
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <div className="navigation">
+            <div className="Link">
+              <Link to="/">Home</Link>
+            </div>
+            <div className="Link">
+              <Link
+                onClick={() => {
+                  this.setState({ visibleLogin: true });
+                }}
+              >
+                LOGIN
+              </Link>
+            </div>
+            <div className="Link">
+              <Link
+                onClick={() => {
+                  this.setState({ visibleSignUp: true });
+                }}
+              >
+                SINGUP
+              </Link>
+            </div>
+          </div>
+          <Route path="/" exact component={Main}></Route>
+          <Modal
+            visible={this.state.visibleLogin}
+            width="600"
+            height="365"
+            effect="fadeInDown"
+            onClickAway={e => {
+              this.setState({ visibleLogin: false });
+            }}
+          >
+            {this.state.visibleLogin ? (
+              <Link to="/Login" component={Login}></Link>
+            ) : (
+              <div>
+                <Route path="/"></Route>
+              </div>
+            )}
+          </Modal>
+          {/* <Route path="/Login" component={Login}></Route> */}
+          {/* <Route path="/SignUp" component={SignUp}></Route> */}
+          <Modal
+            visible={this.state.visibleSignUp}
+            width="800"
+            height="510"
+            effect="fadeInDown"
+            onClickAway={e => {
+              this.setState({ visibleSignUp: false });
+            }}
+          >
+            {this.state.visibleSignUp ? (
+              <Link to="/SignUp" component={SignUp}></Link>
+            ) : (
+              <div>
+                <Route path="/"></Route>
+              </div>
+            )}
+          </Modal>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
-
-export default App;
